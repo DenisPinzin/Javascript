01ed78ac204f66b0983c4868ce1a53d4faba2eee
DenisPinzin <d.pinzinsro@gmail.com>
2025-11-13 09:33:49 +0100

    [NEW] exercice 3 & 5(bis) adjustment

--- algo1/exo-3.js
+++ algo1/exo-3.js
@@ -14,3 +14,19 @@ while (number1 !== 2 || number2 !== 7) {
 
 console.log("number1=" +number1)
 console.log("number2=" +number2)
+
+//----------------------------------- SOLUTION 2
+
+
+let number3 = 7
+let number4 =2
+
+
+let numberTempo1 = number3
+let numberTempo2 = number4
+
+number3 = numberTempo2
+number4 = numberTempo1
+
+console.log("number3 " + number3)
+console.log("number4 " + number4)
---
+++ algo1/exo-5(bis).js
@@ -0,0 +1,12 @@
+/*
+5-bis.Somme multiple personnalisé
+Utilisez l'exercice précédent afin de laisser l'utilisateur choisir le nombre à calculer
+*/
+
+let number = 1
+let choice = 20
+
+for (let i = 0; i <= choice; i++) {
+    number = number + i
+    console.log(number)
+}
