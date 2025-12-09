563f0ef29fa358d2a27c9e8ba63db56fb7eda6a5
DenisPinzin <d.pinzinsro@gmail.com>
2025-12-09 14:54:51 +0100

    [FIX] exo4 remplace innerHTML by value // exo3 window-limit height & width

--- exo-Ludo/exo3/script.js
+++ exo-Ludo/exo3/script.js
@@ -22,4 +22,16 @@ document.addEventListener('keydown', (event) => {
         left -= 10; 
         img.style.left = left + 'px';
     } 
+
+    //SORTIE DE FENETRE
+    const imglarg = img.offsetWidth;
+    const imghaut = img.offsetHeight;
+
+    if (bottom < 0) bottom = 0;
+    if (left < 0) left = 0;
+    if (bottom > window.innerHeight - imghaut) bottom = window.innerHeight - imghaut;
+    if (left > window.innerWidth - imglarg) left = window.innerWidth - imglarg;
+
+    img.style.left = left + 'px';
+    img.style.bottom = bottom + 'px';
 });
--- exo-Ludo/exo4/script.js
+++ exo-Ludo/exo4/script.js
@@ -2,45 +2,41 @@ const h1 = document.querySelector('h1')
 const rgb = document.querySelector('.rgb')
 const rgbRange = document.querySelector('#rgbRange')
 
-rgbRange.addEventListener('mouseup', () => {
-    h1.style.backgroundColor = "rgb(140 140 "+rgb.innerHTML+")"
+rgbRange.addEventListener('input', () => {
+    h1.style.backgroundColor = "rgb(140, 140, " + rgb.value + ")"
 })
 
-const pad =document.querySelector('.pad')
+const pad = document.querySelector('.pad')
 const padRange = document.querySelector('#padRange')
 
-padRange.addEventListener('mouseup', () => {
-    h1.style.padding = pad.innerHTML + "px"
+padRange.addEventListener('input', () => {
+    h1.style.padding = pad.value + "px"
 })
 
-const heig =document.querySelector('.heig')
-const heightRange =document.querySelector('#heightRange')
+const heig = document.querySelector('.heig')
+const heightRange = document.querySelector('#heightRange')
 
-heightRange.addEventListener('mouseup', () => {
-    h1.style.height = heig.innerHTML + "px"
-    console.log(heig.innerHTML )
+heightRange.addEventListener('input', () => {
+    h1.style.height = heig.value + "px"
 })
 
 const widt = document.querySelector('.widt')
 const widthRange = document.querySelector('#widthRange')
 
-widthRange.addEventListener('mouseup', () => {
-    h1.style.width = widt.innerHTML + "px"
-    console.log(widt.innerHTML )
+widthRange.addEventListener('input', () => {
+    h1.style.width = widt.value + "px"
 })
 
-const arron =document.querySelector('.arron')
-const arrondiRange =document.querySelector('#arrondiRange')
+const arron = document.querySelector('.arron')
+const arrondiRange = document.querySelector('#arrondiRange')
 
-arrondiRange.addEventListener('mouseup', () => {
-    h1.style.borderRadius = arron.innerHTML + "px"
-    console.log(arron.innerHTML )
+arrondiRange.addEventListener('input', () => {
+    h1.style.borderRadius = arron.value + "px"
 })
 
-const rotat =document.querySelector('.rotat')
-const rotationRange =document.querySelector('#rotationRange')
+const rotat = document.querySelector('.rotat')
+const rotationRange = document.querySelector('#rotationRange')
 
-rotationRange.addEventListener('mouseup', () => {
-    h1.style.rotate = rotat.innerHTML + "deg"
-    console.log(rotat.innerHTML )
+rotationRange.addEventListener('input', () => {
+    h1.style.rotate = rotat.value + "deg"
 })
