683431460d2e449aa2ed3462d2f705b71557a4c4
DenisPinzin <d.pinzinsro@gmail.com>
2025-12-09 13:05:09 +0100

    [NEW] exercice 4

---
+++ exo-Ludo/exo4/script.js
@@ -0,0 +1,8 @@
+const h1 = document.querySelector('h1')
+const rgb = document.querySelector('.rgb')
+const rgbRange = document.querySelector('#rgbRange')
+
+rgbRange.addEventListener('mouseup', () => {
+    console.log(rgb.innerHTML)
+    h1.style.backgroundColor = 
+})
